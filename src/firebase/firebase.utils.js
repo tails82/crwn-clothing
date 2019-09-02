069264d85df655651f5e954aef87a4c124a9215d
Zhang <wzhang@marinsoftware.com>
2019-09-02 14:47:17 +0800

    implemented firebase utils, include ability to store authenticated users into our firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,34 @@ const config = {
     storageBucket: "",
     messagingSenderId: "98403596475",
     appId: "1:98403596475:web:a866ba4413035d84"
-  };
+};
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+	if (!userAuth) {
+		return;
+	}
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	
+	if(!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt, 
+				...additionalData
+			})
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
+}
 
 firebase.initializeApp(config);
 
---
+++ src/firebase/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firestore.firestore();
+
+firestore.collection("users").doc('CU9Ql6YvMSCf247A7jIk').collection('cartItems').doc('0YcfPCsYwQ4N11A02ZjS');
+firestore.doc('/users/CU9Ql6YvMSCf247A7jIk/cartItems/0YcfPCsYwQ4N11A02ZjS');
+firestore.collection('/users/CU9Ql6YvMSCf247A7jIk/cartItems');
